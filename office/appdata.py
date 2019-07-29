3d34def03f23ff8662af2ee1beccf5fb8b5371c5
matthewgdv <matthewgdv@gmail.com>
2019-07-29 17:17:15 +0100

    Use new pathmagic.Dir.from_appdata() method

--- office/appdata.py
+++ office/appdata.py
@@ -1,5 +1,4 @@
-from appdirs import user_data_dir
 from pathmagic import Dir
 import office
 
-appdata = Dir(user_data_dir(appname=office.__name__, appauthor="python_module_data"))
+appdata = Dir.from_appdata(appname=office.__name__, appauthor="python_module_data")
