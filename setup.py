acebdd6f15e758fd0ba737b8b9fc2212369f9f78
matthewgdv <matthewgdv@gmail.com>
2019-11-15 12:47:12 +0000

    Refactor setup.py to read requirements.txt

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    dependencies = [line for line in f if line]
+
 setup(
     name="office365",
     version=__version__,
@@ -22,9 +25,7 @@ setup(
       "Programming Language :: Python :: 3.7",
     ],
     packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-
-    ],
+    install_requires=dependencies,
     setup_requires=['setuptools_scm'],
     include_package_data=True,
     author="Matt GdV",
